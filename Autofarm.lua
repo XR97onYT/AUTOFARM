fe931204960f40d811befc3b6f5d0c9ea90e97db
XR97onYT <83871996+XR97onYT@users.noreply.github.com>
2021-11-28 13:34:42 -0600

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -206,8 +206,7 @@ function StartAutofarm()
 										TimeLeft = 25
 										game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(v.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 3))
 										wait(0.2)
-										game:GetService("ReplicatedStorage").Events.FallDamage:FireServer(100, v.Character.Hitbox)
-										repeat wait() until not v or not v.Character or v.NRPBS.Health.Value <= 0
+										repeat game:GetService("ReplicatedStorage").Events.FallDamage:FireServer(100, v.Character.Hitbox) wait(.01) until not v or not v.Character or v.NRPBS.Health.Value <= 0
 									end
 								end
 							end
