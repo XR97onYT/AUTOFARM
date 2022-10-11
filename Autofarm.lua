7e5366c863e2e70c0ed0e1bc89d0675f21089e1c
XR97onYT <83871996+XR97onYT@users.noreply.github.com>
2022-10-10 20:13:54 -0500

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -6,7 +6,7 @@
 ]]
 
 if not game:IsLoaded() then game.Loaded:Wait() end
-local versionId = 10219
+local versionId = 10249
 
 local Player = game:GetService("Players").LocalPlayer
 repeat task.wait() until Player:FindFirstChild("DataLoaded")
