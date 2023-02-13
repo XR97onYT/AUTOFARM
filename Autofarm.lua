05c221bebaafbbca88b29cda141306825949f910
XR97onYT <83871996+XR97onYT@users.noreply.github.com>
2023-02-12 20:13:05 -0600

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -254,6 +254,10 @@ else
 							VirtualInput:SendKeyEvent(true, 51, false, game)
 							VirtualInput:SendKeyEvent(false, 51, false, game)
 							LoopCheck77 = true
+						else
+							VirtualInput:SendKeyEvent(true, 49, false, game)
+							VirtualInput:SendKeyEvent(false, 49, false, game)
+							LoopCheck77 = true
 						end
 						LoopCheck7 = tick()
 					else
